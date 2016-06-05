1f53a9470f84fdcf811d2971fdaff088ff0d1a93
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-05 19:56:34 +0200

    Create model Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { in: 10..100 }
+  validates :body, presence: true, length: { in: 10..255 }
 end
--- db/migrate/20160605174918_create_posts.rb
+++ db/migrate/20160605174918_create_posts.rb
@@ -1,8 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    drop_table :posts
+    
     create_table :posts do |t|
       t.string :title
-      t.string :link
+      t.string :body
 
       t.timestamps null: false
     end
